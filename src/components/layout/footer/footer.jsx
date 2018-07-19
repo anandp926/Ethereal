8fbc59b9bdc4b26bed295c75d8f718b98eb67bdd
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-07-19 22:44:52 +0530

    [Fix] indentation and text

--- src/components/layout/footer/footer.jsx
+++ src/components/layout/footer/footer.jsx
@@ -23,93 +23,93 @@ import youtube from './assets/images/social-media-icons/youtube.svg';
 import Logo from '../logo/logo';
 const footer = (props) => {
   return (
-  <footer className="footer">
-    <div className="container container--footer">
-      <div className="footer-box">
-        <div className="logo-col">
-          <Logo url={logoWithName} width={80} height={95} id={"footer-logo"}/>
-          <div className="govt-initiative">
-            <img src={ces} alt="CES" className="ces-logo" height="60"/>
-            <img src={makeInIndia} alt="Make In India" className="make-in-india-logo" height="40"/>
-            <img src={startupIndia} alt="Startup India" className="startup-india-logo" height="25"/>
+    <footer className="footer">
+      <div className="container container--footer">
+        <div className="footer-box">
+          <div className="logo-col">
+            <Logo url={logoWithName} width={80} height={95} id={"footer-logo"}/>
+            <div className="govt-initiative">
+              <img src={ces} alt="CES" className="ces-logo" height="60"/>
+              <img src={makeInIndia} alt="Make In India" className="make-in-india-logo" height="40"/>
+              <img src={startupIndia} alt="Startup India" className="startup-india-logo" height="25"/>
+            </div>
+          </div>
+          <div className="footer-col-divider" content="company-items">
+            <div className="footer-title">Company</div>
+            <ul className="links-list">
+              <li className="link-item">
+                <NavLink to="/home" exact>Home</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to="/about">About Us</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to="/media">Media</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to="/blogs">Blogs</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to="/contact">Contact Us</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to="/careers">Careers</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col-divider" content="product-items">
+            <div className="footer-title">Products</div>
+            <ul className="links-list">
+              <li className="link-item">
+                <NavLink to='/products/ray'>Ray</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to='/products/halo'>Halo</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to='/products/pentagram'>Pentagram</NavLink>
+              </li>
+              <li className="link-item">
+                <NavLink to='/blogs/blog_1'>Concrete 3D Printer</NavLink>
+              </li>
+            </ul>
+          </div>
+          <div className="footer-col-divider" content="social-media">
+            <div className="footer-title">Connect with us</div>
+            <ul className="links-list" type="social-media-list">
+              <li className="link-item link-item--social-media">
+                <a href="https://www.facebook.com/etherealmachines" target="_blank">
+                  <img src={facebook} alt="Facebook" width="30" height="30" />
+                </a>
+              </li>
+              <li className="link-item link-item--social-media">
+                <a href="https://www.instagram.com/ethereal_machines/" target="_blank">
+                  <img src={insta} alt="Instagram" width="30" height="30" />
+                </a>
+              </li>
+              <li className="link-item link-item--social-media">
+                <a href="https://twitter.com/etherealmachine" target="_blank">
+                  <img src={twitter} alt="Twitter" width="30" height="30" />
+                </a>
+              </li>
+              <li className="link-item link-item--social-media">
+                <a href="https://www.linkedin.com/company/etherealmachines/" target="_blank">
+                  <img src={linkedin} alt="Linkedin" width="30" height="30" />
+                </a>
+              </li>
+              <li className="link-item link-item--social-media">
+                <a href="https://www.youtube.com/channel/UCW8I3771Afa4v_KkATxw2EA" target="_blank">
+                  <img src={youtube} alt="YouTube" width="30" height="30" />
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="footer-col-divider" content="company-items">
-          <div className="footer-title">Company</div>
-          <ul className="links-list">
-            <li className="link-item">
-              <NavLink to="/home" exact>Home</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to="/about">About Us</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to="/media">Media</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to="/blogs">Blogs</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to="/contact">Contact Us</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to="/careers">Careers</NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-col-divider" content="product-items">
-          <div className="footer-title">Products</div>
-          <ul className="links-list">
-            <li className="link-item">
-              <NavLink to='/products/ray'>Ray</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to='/products/halo'>Halo</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to='/products/pentagram'>Pentagram</NavLink>
-            </li>
-            <li className="link-item">
-              <NavLink to='/blogs/blog_1'>Concrete 3D Printer</NavLink>
-            </li>
-          </ul>
-        </div>
-        <div className="footer-col-divider" content="social-media">
-          <div className="footer-title">Connect with us</div>
-          <ul className="links-list" type="social-media-list">
-            <li className="link-item link-item--social-media">
-              <a href="https://www.facebook.com/etherealmachines" target="_blank">
-                <img src={facebook} alt="Facebook" width="30" height="30" />
-              </a>
-            </li>
-            <li className="link-item link-item--social-media">
-              <a href="https://www.instagram.com/ethereal_machines/" target="_blank">
-                <img src={insta} alt="Instagram" width="30" height="30" />
-              </a>
-            </li>
-            <li className="link-item link-item--social-media">
-              <a href="https://twitter.com/etherealmachine" target="_blank">
-                <img src={twitter} alt="Twitter" width="30" height="30" />
-              </a>
-            </li>
-            <li className="link-item link-item--social-media">
-              <a href="https://www.linkedin.com/company/etherealmachines/" target="_blank">
-                <img src={linkedin} alt="Linkedin" width="30" height="30" />
-              </a>
-            </li>
-            <li className="link-item link-item--social-media">
-              <a href="https://www.youtube.com/channel/UCW8I3771Afa4v_KkATxw2EA" target="_blank">
-                <img src={youtube} alt="YouTube" width="30" height="30" />
-              </a>
-            </li>
-          </ul>
+        <div className="copyright-box">
+          Copyright &copy; 2018. Ethereal Machines Pvt Ltd. All rights reserved.
         </div>
       </div>
-      <div className="copyright-box">
-        Copyright &copy; 2018. Ethereal Machines Pvt Ltd. All rights reserved
-      </div>
-    </div>
-  </footer>
+    </footer>
   );
 };
 
