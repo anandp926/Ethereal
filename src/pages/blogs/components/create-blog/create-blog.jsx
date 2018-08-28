cca991aa1498afceb4d6315b32660145e552718e
anandp926 <sanand926@gmail.com>
2018-08-28 19:42:40 +0530

    modified: blog-form

--- src/pages/blogs/components/create-blog/create-blog.jsx
+++ src/pages/blogs/components/create-blog/create-blog.jsx
@@ -4,11 +4,24 @@ import Form from '../../../../components/pages-component/form/form'
 import FormControl from '../../../../components/pages-component/form-controls/components/form-control';
 import './create-blog.css'
 import '../../../../components/pages-component/button/button.css'
+import Button from '../../../../components/pages-component/button/button'
 
 class CreateBlog extends Component{
 
     state = {
-        editContent: false
+        editContent: false,
+        formSubmissionStart: false,
+        readOnly: false
+    }
+
+    onSubmitHandle = (e) => {
+        e.preventDefault()
+        this.setState({
+            ...this.state,
+            formSubmissionStart: true,
+            editContent: true,
+            readOnly:true
+          });
     }
 
     render(){
@@ -17,27 +30,51 @@ class CreateBlog extends Component{
                 <section className="section section--blogs">
                     <div className="container">
                         <div className="create-blog-form">
-                            <Form>
+                            <Form classValue="form" onSubmitHandler={this.onSubmitHandle}>
                                 <FormControl>
-                                    <label>
+                                    <label htmlFor="author">
                                         Author
                                         <span className="star"> *</span>
                                     </label>
-                                    <input type="text" className="input-element" placeholder="Enter Author Name"/>
+                                    <input 
+                                        type="text" 
+                                        className="input-element" 
+                                        placeholder="Enter Author Name"
+                                        name="author"
+                                        id="author"
+                                        required={true}
+                                        readOnly={this.state.readOnly}
+                                    />
                                 </FormControl>
                                 <FormControl>
-                                    <label>
+                                    <label htmlFor="title">
                                         Title
                                         <span className="star"> *</span>
                                     </label>
-                                    <input type="text" className="input-element" placeholder="Enter Blog's Title"/>
+                                    <input 
+                                        type="text" 
+                                        className="input-element" 
+                                        placeholder="Enter Blog's Title"
+                                        name="title"
+                                        id="title"
+                                        required={true}
+                                        readOnly={this.state.readOnly}
+                                    />
                                 </FormControl>
                                 <FormControl>
-                                    <label>
+                                    <label htmlFor="thumbnail">
                                         Thumbnail
                                         <span className="star"> *</span>
                                     </label>
-                                    <input type="text" className="input-element" placeholder="Enter Image URL"/>
+                                    <input 
+                                        type="text" 
+                                        className="input-element" 
+                                        placeholder="Enter Image URL"
+                                        name="thumbnail"
+                                        id="thumbnail"
+                                        required={true}
+                                        readOnly={this.state.readOnly}
+                                    />
                                 </FormControl>
                                 <div style={{margin:5}}>
                                     <input
@@ -46,11 +83,22 @@ class CreateBlog extends Component{
                                         id="publish"
                                     />{" "}<span><b>Publish</b></span>
                                 </div>
-                                <button className="form-btn">Save</button>
+                                <Button 
+                                    type="submit" 
+                                    classValue="form-btn" 
+                                    disabled={this.state.formSubmissionStart}
+                                >
+                                    Create
+                                </Button>
                             </Form>
-                            <Link to="/blogs/create/content/id">
-                                <button className="form-btn">Edit Content</button>
-                            </Link>
+                            {
+                                this.state.editContent
+                                ?
+                                <Link to="/blogs/create/content/id">
+                                    <button className="form-btn" >Edit Content</button>
+                                </Link>
+                                : null
+                            }
                         </div>
                     </div>
                 </section>
