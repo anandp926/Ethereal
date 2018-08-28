2d53f5c343dd12568dc1ce9f8b757d5e2e9dc915
anandp926 <sanand926@gmail.com>
2018-08-28 19:15:44 +0530

    added: component for content editor

---
+++ src/pages/blogs/components/create-blog/blog-content-editor/blog-editor.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BlogEditor extends Component{
+    render(){
+        return(
+            <div>
+                Hi This is Anand Singh Parihar
+            </div>
+        )
+    }
+}
+
+export default BlogEditor
