614776ef57ad8dccb1f8e2c32f7566593eba66f9
anandp926 <sanand926@gmail.com>
2018-08-30 11:18:31 +0530

    fix: small bug fix

--- src/pages/blogs/components/create-blog/create-blog.jsx
+++ src/pages/blogs/components/create-blog/create-blog.jsx
@@ -5,13 +5,65 @@ import FormControl from '../../../../components/pages-component/form-controls/co
 import './create-blog.css'
 import '../../../../components/pages-component/button/button.css'
 import Button from '../../../../components/pages-component/button/button'
+import uuid from 'uuid/';
+import createBlog from '../../../../services/api/create-blog'
+import ErrorBox from '../../../../components/pages-component/form/components/error-box';
+import Loader from '../../../../components/ui/loader/loader';
 
 class CreateBlog extends Component{
 
     state = {
+        id: uuid(),
+        author: '',
+        title: '',
+        thumbnail: '',
+        publish: false,
         editContent: false,
         formSubmissionStart: false,
-        readOnly: false
+        readOnly: false,
+        errorMsg: null,
+        btnDisable: false,
+    }
+
+    handleInputChange(e) {
+        this.setState({[e.target.name]: e.target.value})
+        const target = e.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const name = target.name;
+
+        this.setState({
+            [name]: value,
+        });   
+    }
+
+    submitCallback = (data) => {
+        if(data.status === 201){
+            this.setState({
+                editContent: true,
+                readOnly:true,
+                errorMsg: null,
+                formSubmissionStart: false,
+            })
+        }else if (data.response) {
+            this.setState({
+                ...this.state,
+                errorMsg: data.response.data,
+                formSubmissionStart: false,
+                editContent: false,
+                readOnly: false,
+                btnDisable: false
+            });
+        }else {
+        const genericErrorMsg = { Error: ["Oops! Something went wrong, please try again."] };
+        this.setState({
+            ...this.state,
+            errorMsg: genericErrorMsg,
+            formSubmissionStart: false,
+            editContent: false,
+            readOnly: false,
+            btnDisable: false
+        });
+    }
     }
 
     onSubmitHandle = (e) => {
@@ -19,9 +71,34 @@ class CreateBlog extends Component{
         this.setState({
             ...this.state,
             formSubmissionStart: true,
-            editContent: true,
-            readOnly:true
+            btnDisable: true
           });
+        const data = this.createBlogData();
+        if(data){
+            createBlog(this.submitCallback, data)
+        }
+        
+    }
+
+    createBlogData = () => {
+        const { id, author, title, publish, thumbnail } = this.state
+        const data = {
+            id: id,
+            author: author,
+            title: title,
+            thumbnail: thumbnail,
+            publish: publish
+        }
+        return data
+    }
+
+    clearForm = () => {
+        this.setState({
+            author: '',
+            title: '',
+            thumbnail: '',
+            publish: false
+        })
     }
 
     render(){
@@ -44,6 +121,8 @@ class CreateBlog extends Component{
                                         id="author"
                                         required={true}
                                         readOnly={this.state.readOnly}
+                                        value={this.state.author}
+                                        onChange={ e => this.handleInputChange(e)}
                                     />
                                 </FormControl>
                                 <FormControl>
@@ -59,6 +138,8 @@ class CreateBlog extends Component{
                                         id="title"
                                         required={true}
                                         readOnly={this.state.readOnly}
+                                        value={this.state.title}
+                                        onChange={ e => this.handleInputChange(e)}
                                     />
                                 </FormControl>
                                 <FormControl>
@@ -74,6 +155,8 @@ class CreateBlog extends Component{
                                         id="thumbnail"
                                         required={true}
                                         readOnly={this.state.readOnly}
+                                        value={this.state.thumbnail}
+                                        onChange={ e => this.handleInputChange(e)}
                                     />
                                 </FormControl>
                                 <div style={{margin:5}}>
@@ -81,12 +164,16 @@ class CreateBlog extends Component{
                                         type="checkbox"
                                         name="publish"
                                         id="publish"
+                                        onChange={ e => this.handleInputChange(e)}
+                                        checked={this.state.publish}
                                     />{" "}<span><b>Publish</b></span>
                                 </div>
+                                { this.state.errorMsg ? <ErrorBox isRequired errorMsg={this.state.errorMsg} /> : null}
+                                { this.state.formSubmissionStart ? <Loader>Submitting your request...</Loader> : null }
                                 <Button 
                                     type="submit" 
                                     classValue="form-btn" 
-                                    disabled={this.state.formSubmissionStart}
+                                    disabled={this.state.btnDisable}
                                 >
                                     Create
                                 </Button>
@@ -94,7 +181,7 @@ class CreateBlog extends Component{
                             {
                                 this.state.editContent
                                 ?
-                                <Link to="/blogs/create/content/id">
+                                <Link to={`/blogs/create/content/${this.state.id}`}>
                                     <button className="form-btn" >Edit Content</button>
                                 </Link>
                                 : null
