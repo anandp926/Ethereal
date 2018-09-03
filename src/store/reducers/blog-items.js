ba0cc667476aa4e9b1d065709033058e1302582c
anandp926 <sanand926@gmail.com>
2018-09-03 18:52:41 +0530

    modified: reducers

--- src/store/reducers/blog-items.js
+++ src/store/reducers/blog-items.js
@@ -5,13 +5,52 @@
 
 /* This is the Reducer for the Blog Items */
 
+import * as actionType from '../actions/action-type'
+
 const initialState = {
   firstRun: true,
-  blogItems: []
+  blogItems: [],
+  contentOpen: false,
+  blogItemsById: []
 };
 
 const reducer = (state = initialState, action) => {
-  return state;
+  switch(action.type){
+    case actionType.BLOG_ITEM:
+    return{
+      ...state,
+      blogItems: action.value
+    }
+    case actionType.BLOG_ITEM_BY_ID:
+    return{
+      ...state,
+      blogItemsById:action.value
+    }
+    case actionType.ADD_BLOG_ITEM:
+    return{
+      ...state,
+      blogItems: state.blogItems.concat(action.value)
+    }
+    case actionType.UPDATE_BLOG_ITEM:{
+      return Object.assign({}, state, {
+          blogItems : state.blogItems.map(blog => {
+              if (blog.id !== action.value.id) {
+          return blog;
+      }
+      return Object.assign({}, blog, {
+          content: action.value.content,
+      })
+  })
+  });
+  }
+    case actionType.BLOG_CONTENT_OPEN:
+    return{
+      ...state,
+      contentOpen: action.value
+    }
+    default:
+      return state
+  }
 };
 
 export default reducer;
