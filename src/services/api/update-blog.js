7c20a3345c8a580d76c03ec4673a80774473a7d7
anandp926 <sanand926@gmail.com>
2018-09-03 18:56:21 +0530

    added: api for update blog by id

---
+++ src/services/api/update-blog.js
@@ -0,0 +1,24 @@
+
+/**
+ * Created by Anand Singh on 8/30/2018.
+ */
+import  axios  from '../axios/axios';
+import headers from '../axios/headers';
+
+const updateblog = (callback,id,data) => {
+    axios.patch(`/blogs/${id}/`,data, {
+            headers: {
+                ...headers,
+            'Content-Type': 'application/json'
+        }
+}).then(res => {
+    console.log(res)
+        callback(res)
+    })
+.catch(err => {
+    console.log(err)
+    callback(err)
+})
+};
+
+export default updateblog
