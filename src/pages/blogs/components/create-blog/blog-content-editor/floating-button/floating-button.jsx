cabe87a5f63b4eff36b528076c550de7089fd313
Anand Singh <sanand926@gmail.com>
2018-09-06 11:32:41 +0530

    modified: added upload image functionlity and small bug fixed

--- src/pages/blogs/components/create-blog/blog-content-editor/floating-button/floating-button.jsx
+++ src/pages/blogs/components/create-blog/blog-content-editor/floating-button/floating-button.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {connect} from 'react-redux'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -12,6 +13,10 @@ import Tooltip from '@material-ui/core/Tooltip';
 import Modal from 'react-modal'
 import ImageLayout from '../image-layout/image-layout'
 import './floating-button.css'
+import uploadImage from '../../../../../../services/api/uploadImage'
+import Loader from '../../../../../../components/ui/loader/loader'
+import ErrorBox from '../../../../../../components/pages-component/form/components/error-box'
+import * as actionType from '../../../../../../store/actions/action-type'
 
 
 const styles = theme => ({
@@ -36,27 +41,92 @@ const styles = theme => ({
     zIndex: 1
   },
   tooltips: {
-      maxWidth:150,
-      height: 100,
-      fontSize: 50,
+      maxWidth:200,
+      height: 30,
+      fontSize: 15,
+      textAlign: 'center',
+      backgroundColor:'black',
+      borderRadius:0
   },
 });
 
 class FloatingActionButtonZoom extends React.Component {
+    constructor(props) {
+        super(props);
+        this.imageSize = null;
+        this.data = new FormData();
+      }
 
     state = {
-        showModal: false
+        showModal: false,
+        errorMsg: null,
+        formSubmissionStart: false,
     }
 
     insertImageLink = () => {
-        this.setState({showModal: true})
+        
+        this.setState({
+            showModal: true
+        })
         
     }
     
     closeModal = () =>{
-        this.setState({showModal: false})
+        this.data.delete('images')
+        this.setState({
+            showModal: false,
+            errorMsg: null,
+            formSubmissionStart: false,
+        })
     }
 
+    imageCallback = (data) => {
+        if(data.status === 201){
+            this.props.addImages(data.data)
+            this.setState({
+                ...this.state,
+                errorMsg: null,
+                formSubmissionStart: false
+            });
+        }else if (data.response) {
+                this.setState({
+                    ...this.state,
+                    errorMsg: data.response.data,
+                    formSubmissionStart: false
+                });
+            }else {
+            const genericErrorMsg = { Error: ["Oops! Something went wrong, please try again."] };
+            const fileErrorMsg = { Resume: ["Max. limit for resume 5 MB"] };
+            this.setState({
+                ...this.state,
+                errorMsg: this.fileSize > 5 ? fileErrorMsg : genericErrorMsg,
+                formSubmissionStart: false
+            });
+        }
+    }
+
+    onSubmitHandler = (e) => {
+        e.preventDefault()
+        this.setState({
+            ...this.state,
+            formSubmissionStart: true
+          });
+        if(this.data){
+            const imgData = this.data
+            uploadImage(this.imageCallback, imgData)
+            }
+               
+    }
+
+    inputChange = (e) => {
+        this.imageSize = (e.target.files[0].size / (1024*1024)).toFixed(0)
+        this.setState({images: e.target.files[0]})
+        let file = e.target.files[0];
+        
+        if (file) {
+          this.data.append('images', file);
+        }
+    }
 
   render() {
     const { classes } = this.props;
@@ -67,24 +137,35 @@ class FloatingActionButtonZoom extends React.Component {
             isOpen={this.state.showModal}
             onRequestClose={this.closeModal}
             className="modalStyle"
+            ariaHideApp={false}
             >
             <div className="modalContent">
                 <div className="imageLayout">
                     <ImageLayout/>
-                    <ImageLayout/>
                 </div>
                 <div className="imageUpload">
-                    <form>
+                    <form onSubmit={this.onSubmitHandler}>
                         <div className="form-control">
                             <label htmlFor="img">
                                 Upload Image To Get URL
                                 <span className="star"> *</span>
                             </label>
-                            <input type="file" className="input-element file-input" name="img" id="img"/>
+                            <input 
+                                type="file" 
+                                className="input-element file-input" 
+                                name="images" 
+                                id="img"
+                                onChange={e => this.inputChange(e)}
+                                accept=".jpeg, .jpg, .png" 
+                                required={true}   
+                            />
                         </div>
+                        { this.state.errorMsg ? <ErrorBox isRequired errorMsg={this.state.errorMsg} /> : null}
+                        { this.state.formSubmissionStart ? <Loader>Submitting your request...</Loader> : null }
+                    
                         <div className="upload">
-                            <button className="btn" className="upload-btn">Uplaod</button>
-                            <button className="btn" className="upload-btn" onClick={this.closeModal}>cancel</button>
+                            <button className="upload-btn" disabled={this.state.formSubmissionStart} >Uplaod</button>
+                            <button className="upload-btn" onClick={this.closeModal}>cancel</button>
                         </div>
                     </form>
                 </div>
@@ -92,14 +173,14 @@ class FloatingActionButtonZoom extends React.Component {
             </Modal>
             <List className={classes.list}>
                 <ListItem>
-                    <Tooltip title="Save" placement="left" className={classes.tooltips} >
+                    <Tooltip title="Save" placement="left" classes={{ tooltip: classes.tooltips }}>
                         <Button variant="fab" className={classes.fabGreen} color= 'inherit' onClick={() => this.props.onUpdate()}>
                             <Save />
                         </Button>
                     </Tooltip>
                 </ListItem>
                 <ListItem>
-                    <Tooltip title="Insert Image Link" placement="left">
+                    <Tooltip title="Insert Image Link" placement="left" classes={{ tooltip: classes.tooltips }}>
                         <Button variant="fab" className={classes.fabBlue} color= 'inherit' onClick={this.insertImageLink}>
                             <PhotoLibrary />
                         </Button>
@@ -116,4 +197,16 @@ FloatingActionButtonZoom.propTypes = {
   theme: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles, { withTheme: true })(FloatingActionButtonZoom);
+const mapDispatchToProps = (dispatch) => {
+    return{
+      addImages:  (data) => {
+        dispatch({
+          type:actionType.ADD_IMAGE_GALLARY,
+          value: data
+        })
+      }
+    }
+  }
+
+
+export default withStyles(styles, { withTheme: true })(connect(null, mapDispatchToProps)(FloatingActionButtonZoom));
