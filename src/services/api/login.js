92fcb1ba80294ed591ade252c910f0ac7cc31c40
Anand Singh <sanand926@gmail.com>
2018-09-06 11:37:52 +0530

    updated: base url

--- src/services/api/login.js
+++ src/services/api/login.js
@@ -4,7 +4,7 @@
 import axios from 'axios';
 
 const loginInstance = axios.create({
-    baseURL: 'http://192.168.1.114:8000'
+    baseURL: 'http://192.168.1.109:8000'
 });
 
 const loginUser = (callback,data) => {
