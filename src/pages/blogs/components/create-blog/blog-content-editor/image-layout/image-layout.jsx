5cd983856b96ea1fc665718943249e4b9aa739bb
Anand Singh <sanand926@gmail.com>
2018-09-06 11:31:03 +0530

    modified: added copy clipBoard text functionality

--- src/pages/blogs/components/create-blog/blog-content-editor/image-layout/image-layout.jsx
+++ src/pages/blogs/components/create-blog/blog-content-editor/image-layout/image-layout.jsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {connect} from 'react-redux'
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import './image-layout.css'
+import getGallary from '../../../../../../services/api/get-gallary'
+import * as actionType from '../../../../../../store/actions/action-type'
 
 const styles = theme => ({
   root: {
@@ -19,59 +22,83 @@ const styles = theme => ({
     height: 500,
   },
 });
-
-
-  const tileData = [
-    {
-      img: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26h%3D350&imgrefurl=https://www.pexels.com/search/beauty/&h=350&w=528&tbnid=ZAXhvwKefKr6jM:&q=image&tbnh=132&tbnw=200&usg=AFrqEzdfO1R_OkF0ZLQQlVQqkxn4WZ3W6Q&vet=1&docid=pFs_4Fcq5AgpmM&itg=1&sa=X&ved=2ahUKEwiHls7Djo_dAhXUfH0KHUthCcgQ_B0wG3oECAgQCQ",
-      title: 'Image',
-      id:1
-    },
-    {
-    img: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26h%3D350&imgrefurl=https://www.pexels.com/search/beauty/&h=350&w=528&tbnid=ZAXhvwKefKr6jM:&q=image&tbnh=132&tbnw=200&usg=AFrqEzdfO1R_OkF0ZLQQlVQqkxn4WZ3W6Q&vet=1&docid=pFs_4Fcq5AgpmM&itg=1&sa=X&ved=2ahUKEwiHls7Djo_dAhXUfH0KHUthCcgQ_B0wG3oECAgQCQ",
-    title: 'Image',
-    id:2
-    },
-    {
-    img: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26h%3D350&imgrefurl=https://www.pexels.com/search/beauty/&h=350&w=528&tbnid=ZAXhvwKefKr6jM:&q=image&tbnh=132&tbnw=200&usg=AFrqEzdfO1R_OkF0ZLQQlVQqkxn4WZ3W6Q&vet=1&docid=pFs_4Fcq5AgpmM&itg=1&sa=X&ved=2ahUKEwiHls7Djo_dAhXUfH0KHUthCcgQ_B0wG3oECAgQCQ",
-    title: 'Image',
-    id:3
-    },
-    {
-    img: "https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto%3Dcompress%26cs%3Dtinysrgb%26h%3D350&imgrefurl=https://www.pexels.com/search/beauty/&h=350&w=528&tbnid=ZAXhvwKefKr6jM:&q=image&tbnh=132&tbnw=200&usg=AFrqEzdfO1R_OkF0ZLQQlVQqkxn4WZ3W6Q&vet=1&docid=pFs_4Fcq5AgpmM&itg=1&sa=X&ved=2ahUKEwiHls7Djo_dAhXUfH0KHUthCcgQ_B0wG3oECAgQCQ",
-    title: 'Image',
-    id:4
-    }
-  ];
  
-function TitlebarGridList(props) {
-  const { classes } = props;
+  class ImageLayout extends Component{
 
-  return (
-    <div className={classes.root}>
-      <GridList cellHeight={200} className={classes.gridList}>
-        {tileData.map(tile => (
-          <GridListTile key={tile.id}>
-            <img src={tile.img} alt={tile.title} />
-            <GridListTileBar
-              subtitle={
-                <div className="InputAddOn">
-                    <span>
-                    <button className="image-layout-btn">copy</button>
-                    </span>
-                    <input type="text" className="InputAddOn-field"/>
-                </div>
-                }
-            />
-          </GridListTile>
-        ))}
-      </GridList>
-    </div>
+    state = {
+      loading: true
+    }
+
+    gallaryCallback = (data) => {
+      if(data.status === 200){
+        this.props.getImages(data.data)
+        this.setState({
+          loading:false
+        })
+      }else{
+        console.log(data)
+      }
+    }
+
+    componentDidMount(){
+      getGallary(this.gallaryCallback)
+    }
+
+    copyUrl = (id) => {
+      var copyText = document.getElementById(id);
+      copyText.select();
+      document.execCommand("copy");
+    }
+
+    render(){
+      const { classes } = this.props;
+      return (
+      <div className={classes.root}>
+        <GridList cellHeight={200} className={classes.gridList}>
+          {
+            this.props.gallaryItems !== undefined &&(
+              this.props.gallaryItems.map(tile => (
+                <GridListTile key={tile.id}>
+                  <img src={tile.images} alt={tile.title} />
+                  <GridListTileBar
+                    subtitle={
+                      <div className="InputAddOn">
+                          <span>
+                          <button className="image-layout-btn" onClick={() => this.copyUrl(tile.id)}>copy</button>
+                          </span>
+                          <input type="text" className="InputAddOn-field" id={tile.id} value={tile.images} readOnly={true}/>
+                      </div>
+                      }
+                  />
+                </GridListTile>
+              ))
+            )
+          }
+        </GridList>
+      </div>
   );
-}
+    }
+  }
 
-TitlebarGridList.propTypes = {
+  ImageLayout.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(TitlebarGridList);
+const mapStateToProps = (state) => {
+  return{
+    gallaryItems: state.gallary.gallaryItems,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return{
+    getImages:  (data) => {
+      dispatch({
+        type:actionType.IMAGE_GALLARY,
+        value: data
+      })
+    }
+  }
+}
+
+export default withStyles(styles)(connect(mapStateToProps,mapDispatchToProps)(ImageLayout));
