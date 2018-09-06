c8bcf079fa49348338347d46f64a1ddc808aebb0
Anand Singh <sanand926@gmail.com>
2018-09-06 11:23:30 +0530

    modified: added functional ui for modification and removed navlink

--- src/pages/blogs/components/blog-item/blog-item.jsx
+++ src/pages/blogs/components/blog-item/blog-item.jsx
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 //import calendarIcon from '../../../../assets/images/calendar-icon.svg';
 
@@ -13,25 +13,47 @@ import './blog-item.css';
 const blogItem = (props) => {
   return (
     <div className="blog-item">
-      <NavLink to={`blogs/blog/${props.id}`} exact>
-        <div className="blog-img-container">
-          <img src={props.imgUrl} alt="Blog Item" width="400" height="300"/>
+      <div className="blog-img-container">
+        <img src={props.imgUrl} alt="Blog Item" width="400" height="300"/>
+      </div>
+      <div className="blog-details-container">
+        <div className="blog-title subtitle">
+          <span>{props.title}</span>
         </div>
-        <div className="blog-details-container">
-          <div className="blog-title subtitle">
-            <span>{props.title}</span>
-          </div>
-          {/* <div className="blog-date flex-row">
-            <img src={calendarIcon} alt="Calender" width="16" height="16" className="icon"/>
-            <span>Published On:</span>
-            <span>{props.date}</span>
-          </div> */}
-          <div className="blog-link">
-            {/* <NavLink to={`blogs/blog/${props.id}`} exact>Read More</NavLink> */}
-            <p>Read More</p>
-          </div>
+        {/* <div className="blog-date flex-row">
+          <img src={calendarIcon} alt="Calender" width="16" height="16" className="icon"/>
+          <span>Published On:</span>
+          <span>{props.date}</span>
+        </div> */}
+        <div className="blog-link">
+        {
+          props.session
+          ?
+          <ul className="display-list">
+            <li>
+                <Link to={`blogs/blog/${props.id}`} >Read More</Link>
+            </li>
+            {/* <li>
+                <a >Update</a>
+            </li> */}
+            <li>
+              {
+                  props.publish
+                  ?
+                      <a onClick={() => props.unpublishBlog(props.id,false)} >UnPublish</a>
+                  :
+                      <a onClick={() => props.publishBlog(props.id,true)} >Publish</a>
+              }
+            </li>
+            <li>
+              <a onClick={() => props.deleteBlog(props.id)}>Delete</a>
+            </li>
+          </ul>
+          :
+          <Link to={`blogs/blog/${props.id}`} >Read More</Link>
+        }
         </div>
-      </NavLink>
+      </div>
     </div>
   );
 };
