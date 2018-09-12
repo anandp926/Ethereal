d19769bba8c17c03bf562c041f7c25c996f4e5c9
Anand Singh <sanand926@gmail.com>
2018-09-12 12:38:49 +0530

    added:default empty data

---
+++ src/pages/blogs/components/blog-page/blog-empty-data.js
@@ -0,0 +1,47 @@
+const data = {
+    "id": "c53a6678-cbdd-4b21-baf9-f4d37ca66c54",
+    "cells": [
+      {
+        "id": "3e25baad-51e3-4501-8088-b6d7b8bf84f7",
+        "inline": null,
+        "size": 12,
+        "content": {
+          "plugin": {
+            "name": "ory/editor/core/content/slate",
+            "version": "0.0.2"
+          },
+          "state": {
+            "serialized": {
+              "object": "value",
+              "document": {
+                "object": "document",
+                "data": {},
+                "nodes": [
+                  {
+                    "object": "block",
+                    "type": "PARAGRAPH/PARAGRAPH",
+                    "isVoid": false,
+                    "data": {},
+                    "nodes": [
+                      {
+                        "object": "text",
+                        "leaves": [
+                          {
+                            "object": "leaf",
+                            "text": "comming soon...",
+                            "marks": []
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          }
+        }
+      }
+    ]
+  }
+
+  export default data
