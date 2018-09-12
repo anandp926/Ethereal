3eb358d3db22f584796edd128cc8ad77e31d7815
Anand Singh <sanand926@gmail.com>
2018-09-12 12:47:03 +0530

    modified: changed base url

--- src/services/axios/axios.js
+++ src/services/axios/axios.js
@@ -6,7 +6,7 @@
  import axios from 'axios';
 
  const instance = axios.create({
-   baseURL: 'http://192.168.1.109:8000/v1'
+   baseURL: 'http://192.168.1.105:8000/v1'
  });
 
  export default instance;
