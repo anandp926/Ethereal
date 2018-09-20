b4847165a0c3501dc373fc46e5ec298664113b37
Anand Singh <sanand926@gmail.com>
2018-09-20 13:44:01 +0530

    updated: base url

--- src/services/axios/axios.js
+++ src/services/axios/axios.js
@@ -6,7 +6,7 @@
  import axios from 'axios';
 
  const instance = axios.create({
-   baseURL: 'http://192.168.1.105:8000/v1'
+   baseURL: 'https://api.etherealmachines.com/v1'
  });
 
  export default instance;
